988c7a1956a6f26138810577b619274e405c368a
karelz-47 <karel.zvolsky@gmail.com>
2025-02-03 09:19:17 +0100

    Update SLchat_v7.py

--- SLchat_v7.py
+++ SLchat_v7.py
@@ -143,8 +143,9 @@ if submit_button and user_input:
             "model": selected_model,
             "messages": st.session_state['messages']
         }
-        if not selected_model.startswith("o3"):
+        iif not (selected_model.startswith("o1") or selected_model.startswith("o3")):
             api_params["temperature"] = temperature
+        
         api_params.update(token_param)
 
         try:
